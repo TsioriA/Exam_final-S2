5e6f78922839876ce90bb971b2c44ab40ed1d416
Andriamalala <tsioriandria03@gmail.com>
2025-07-14 13:24:11 +0100

    v2

--- index.php
+++ index.php
@@ -1,19 +1,3 @@
 <?php
-include ('inc/connexion.php');
+    header("location: pages/login.php");
 ?>
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Accueil - EXAM_FINAL-S2</title>
-    <link rel="stylesheet" href="assets/css/style.css" />
-     <link href="assets/bootstrap-5.3.5-dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="assets/bootstrap-5.3.5-dist/js/bootstrap.bundle.min.js"></script>
-</head>
-<body>
-    <?php include('inc/menu.php'); ?>
-    <h1>Bienvenue sur le site d'emprunt</h1>
-</body>
-</html>
--- pages/login.php
+++ pages/login.php
@@ -1,14 +1,14 @@
 <?php
- include('inc/connexion.php');
+ include('../inc/connexion.php');
  ?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
     <title>Connexion</title>
-    <link rel="stylesheet" href="assets/css/style.css">
-    <link href="assets/bootstrap-5.3.5-dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="assets/bootstrap-5.3.5-dist/js/bootstrap.bundle.min.js"></script>
+     <link rel="stylesheet" href="../assets/css/style.css">
+     <link href="../assets/bootstrap-5.3.5-dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="../assets/bootstrap-5.3.5-dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
     <h1>Connexion</h1>
